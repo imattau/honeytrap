0991cabcb4c4d471cc5794e1e03da92d5d1b36e3
imattau <imatt.au@protonmail.com>
2026-02-01 13:02:58 +1100

    Add PWA runtime caching

--- vite.config.ts
+++ vite.config.ts
@@ -14,11 +14,59 @@ export default defineConfig({
         'assets/honeytrap_logo_64.png',
         'assets/honeytrap_logo_192.png',
         'assets/honeytrap_logo_256.png',
-        'assets/honeytrap_logo_512.png'
+        'assets/honeytrap_logo_512.png',
+        'offline.html'
       ],
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,woff2}'],
-        navigateFallback: '/index.html'
+        navigateFallback: '/index.html',
+        runtimeCaching: [
+          {
+            urlPattern: ({ request }) => request.mode === 'navigate',
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'honeytrap-pages',
+              networkTimeoutSeconds: 3,
+              expiration: {
+                maxEntries: 20,
+                maxAgeSeconds: 60 * 60 * 24
+              }
+            }
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'honeytrap-images',
+              expiration: {
+                maxEntries: 200,
+                maxAgeSeconds: 60 * 60 * 6
+              }
+            }
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'video',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'honeytrap-media',
+              expiration: {
+                maxEntries: 60,
+                maxAgeSeconds: 60 * 60 * 6
+              }
+            }
+          },
+          {
+            urlPattern: ({ request }) => request.destination === 'font',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'honeytrap-fonts',
+              expiration: {
+                maxEntries: 20,
+                maxAgeSeconds: 60 * 60 * 24 * 365
+              }
+            }
+          }
+        ]
       },
       manifest: {
         name: 'Honeytrap',
